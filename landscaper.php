ba4221c5a473db5f1071ee9af4157043a89ed659
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2023-11-02 17:07:54 -0500

    landscaper: user can use teeth to cut grass

--- landscaper.php
+++ landscaper.php
@@ -1,6 +1,17 @@
 <?php 
 
+    $balance = 0;
 
+
+    echo "Start Mowing! \n";
+    echo "Press 1 to start chewing the grass!";
+
+    $response = trim(fgets(STDIN));
+
+    if ($response == 1) {
+        $balance++;
+        echo "You now have $" . $balance;
+    }
 
 
 ?>
